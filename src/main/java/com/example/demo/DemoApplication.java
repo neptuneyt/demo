08498a7e7c10ed876e693dbe30e57a78d4e1de6d
Zou Dong <zoud@big.ac.cn>
2018-12-06 10:51:55 +0800

    #add annotation

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,8 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+
+//包含@Configuration、@EnableAutoConfiguration、@ComponentScan，通常用在主类上
 @SpringBootApplication
 public class DemoApplication{
+
+	//应用运行主程序
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
--- src/main/java/com/example/demo/controller/BasicController.java
+++ src/main/java/com/example/demo/controller/BasicController.java
@@ -1,26 +1,36 @@
 package com.example.demo.controller;
 
 import com.example.demo.dao.SpeciesDAO;
+import com.example.demo.model.Species;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * @Author: zoud
  * @Date: 2018/12/3
  **/
 
-@Controller
-@RequestMapping("/")
+
+@Controller /*注解作用：告诉程序这是一个控制组件*/
+@RequestMapping("/") /*地址映射，方法中所有映射的父路径*/
 public class BasicController {
 
     @Autowired
     private SpeciesDAO speciesDAO;
 
-    @RequestMapping({"", "index", "home"})
+    @RequestMapping({"", "index", "home"}) /*将地址映射集合*/
     public String home(ModelMap modelMap){
-        modelMap.put("list", speciesDAO.findAll());
+        //查询所有物种，放在List中
+        List<Species> speciesList = speciesDAO.findAll();
+
+        //将查询结果保存在一个map中，传入前台页面
+        modelMap.put("list", speciesList);
+
+        //返回index.jsp
         return "index";
     }
 }
