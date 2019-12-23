3c12d17560dc2d2046e2fb02607af3f774ede2e9
Zou Dong <zoud@big.ac.cn>
2019-12-23 14:28:01 +0800

    #FC

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,8 +2,6 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 //包含@Configuration、@EnableAutoConfiguration、@ComponentScan，通常用在主类上
